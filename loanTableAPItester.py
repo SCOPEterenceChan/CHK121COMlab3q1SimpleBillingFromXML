9d80022e0a0fe1cd2413b499cf2d66906c2b00d9
SCOPEterenceChan <dcywchan@cityu.edu.hk>
2021-04-23 17:01:23 +0800

    input error

--- loanTableAPItester.py
+++ loanTableAPItester.py
@@ -2,5 +2,8 @@ import sys
 import loanTableAPI
 
 if __name__ == "__main__":
-    loanTableAPI.loanTable(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
-    loanTableAPI.loanTableHTML(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
+    try:
+        loanTableAPI.loanTable(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
+        loanTableAPI.loanTableHTML(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
+    except ValueError as valueError:
+        print(valueError,file=sys.stderr,flush=True)
